7e60a511443f5b1ff86c4c221ebb4ec050cb6786
thomastai1666 <36852809+thomastai1666@users.noreply.github.com>
2021-10-23 13:31:15 -0400

    #11: Create header component

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,12 +1,26 @@
-import './Header.css';
+import "./Header.css";
+import Logo from "./Logo";
+import InformationIcon from "./InformationIcon";
+import { Link } from "react-router-dom";
 
-import React from 'react';
+import React from "react";
 
 function Header(props) {
   return (
-    <div className="Header">
-    </div>
+    <header className="Header">
+      <div className="menu-button"></div>
+      <div>
+        <Link to="/">
+          <Logo height={50} />
+        </Link>
+      </div>
+      <div className="menu-button">
+        <Link to="/team" className="menu-button-link">
+          <InformationIcon />
+        </Link>
+      </div>
+    </header>
   );
-};
+}
 
 export default Header;
