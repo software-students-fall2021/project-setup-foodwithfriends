1dd3a3b8c922d2d46ea673b39d44955807560811
Tanya Singh <ts3516@nyu.edu>
2021-10-27 04:05:03 -0400

    Created team page and added styling to the elements. Still need to properly align the elements for mobile screens

--- front-end/src/pages/TeamPage.jsx
+++ front-end/src/pages/TeamPage.jsx
@@ -1,11 +1,57 @@
 import './TeamPage.css';
-
+import Logo from '../components/Logo.jsx'; 
 import React from 'react';
+import BackButton from '../components/BackButton';
 
 function TeamPage(props) {
   return (
     <div className="TeamPage">
-      Team
+        <div id="full-page">
+            <div id="back-button">
+              <BackButton/>
+            </div>
+            <div id="logo-comp">
+              <Logo/>
+            </div>
+            <div id="introduction-title">
+                Introduction
+            </div>
+            <div id="introduction-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </div>
+            <div id="the-team-title">
+                The Team 
+            </div>
+            <div id = "photo-container">
+                <div class="flex-container">
+                    <div id = "photo-1"> 
+                      <div id = "photo-1-title"> Person 1 </div>
+                    </div>
+                    <div id = "photo-2"> 
+                      <div id="photo-2-title"> Person 2 </div>
+                    </div>
+                </div>
+                <div class="flex-container">
+                    <div id = "photo-3"> 
+                      <div id="photo-3-title"> Person 3 </div>
+                    </div> 
+                    <div id = "photo-4"> 
+                      <div id="photo-4-title"> Person 4 </div>
+                    </div>
+                </div>
+                <div class="flex-container">
+                    <div id = "photo-5"> 
+                      <div id="photo-5-title"> Person 5 </div>
+                    </div>
+                    <div id = "photo-6"> 
+                      <div id="photo-6-title"> Person 6 </div>
+                    </div>
+                </div>
+            </div>
+            <div id="try-now-button">
+              <button id="try-button"> Try Now </button>
+            </div>
+        </div>
     </div>
   );
 };
