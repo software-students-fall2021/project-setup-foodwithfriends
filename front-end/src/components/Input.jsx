9c9a2d0980cda9dfc8b74746b82d0851ae9eefa2
Jen-Lopez <jennlolopz@gmail.com>
2021-10-27 21:24:05 -0400

    split lines

--- front-end/src/components/Input.jsx
+++ front-end/src/components/Input.jsx
@@ -5,7 +5,10 @@ import React from 'react';
 function Input(props) {
   return (
     <div className="input">
-      <label for={props.labelFor}> <span>{props.label}</span> <input type="text" name={props.inputName}/></label>
+      <label htmlFor={props.labelFor}> 
+        <span>{props.label}</span> 
+        <input type="text" name={props.inputName}/>
+      </label>
     </div>
   );
 };
