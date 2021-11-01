52983e7c196cee39ee3e2e8bfe43929abefeaf00
Jen-Lopez <jennlolopz@gmail.com>
2021-11-01 00:38:59 -0400

    create waiting room

---
+++ front-end/src/pages/Wait.jsx
@@ -0,0 +1,26 @@
+import './Wait.css';
+
+import React from 'react';
+
+const fake_users = [{name: "Jen"}, {name: "Tanya"}, {name: "Thomas"}];
+const total = 6;
+
+function Wait(props) {
+  return (
+    <div className="Wait">
+      <h1>Waiting Room</h1>
+      <p id="total">{fake_users.length}/{total} Participants</p>
+      <div id="users">
+        {fake_users.map( (user, i) =>  {
+          const initial = user.name.charAt(0);
+          return <div className="user-item" key={i}>
+            <span className="icon"> <span className="initial">{initial}</span> </span>
+            <p>{user.name}</p>
+          </div>;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Wait;
