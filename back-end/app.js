4a96c95fe036e57357552011c28304c51aeb8098
Yoon_Koh <bear04012@gmail.com>
2021-11-01 11:54:59 -0400

    Set up backend

---
+++ back-end/app.js
@@ -0,0 +1,23 @@
+// app.js
+const mongoose = require('mongoose');
+const express = require('express');
+const session = require('express-session');
+const app = express();
+require('./utils/database.js');
+
+app.use(express.urlencoded({ extended: true }));
+const sessionOptions = {
+	secret: 'secret for signing session id',
+	saveUninitialized: false,
+	resave: false
+};
+
+app.use(session(sessionOptions));
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', function(req, res) {
+  res.send('test');
+});
+
+
+app.listen(3000);
