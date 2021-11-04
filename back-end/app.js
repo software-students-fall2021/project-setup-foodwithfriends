f182527f66d915bfbbc5a9406593595818be5cbb
thomastai1666 <36852809+thomastai1666@users.noreply.github.com>
2021-11-04 00:21:47 -0400

    #90: Add testing libraries + npm scripts

--- back-end/app.js
+++ back-end/app.js
@@ -1,23 +1,22 @@
 // app.js
-const mongoose = require('mongoose');
-const express = require('express');
-const session = require('express-session');
+const mongoose = require("mongoose");
+const express = require("express");
+const session = require("express-session");
 const app = express();
-require('./utils/database.js');
+require("./utils/database.js");
 
 app.use(express.urlencoded({ extended: true }));
 const sessionOptions = {
-	secret: 'secret for signing session id',
-	saveUninitialized: false,
-	resave: false
+  secret: "secret for signing session id",
+  saveUninitialized: false,
+  resave: false,
 };
 
 app.use(session(sessionOptions));
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', function(req, res) {
-  res.send('test');
+app.get("/", function (req, res) {
+  res.send("test");
 });
 
-
-app.listen(3000);
+app.listen(8000);
