c32421a019d172bf17b3891ad278802e3cd548d8
Jen-Lopez <jennlolopz@gmail.com>
2021-11-08 01:36:02 -0500

    implement google search

--- front-end/src/pages/CreateRoom.jsx
+++ front-end/src/pages/CreateRoom.jsx
@@ -5,6 +5,7 @@ import { useHistory } from "react-router-dom";
 import RoomButton from "../components/RoomButton";
 import Spacer from "../components/Spacer";
 import { room_post } from "../utils/api";
+import { useEffect } from 'react';
 
 const MAX_CAPACITY = 20;
 const MIN_CAPACITY = 2;
@@ -20,6 +21,60 @@ function CreateRoom() {
     const roomId = data.roomId;
     history.push(`/invite`, { roomId: roomId });
   };
+
+  useEffect(() => {
+    // load google scripts dynamically
+    const gScript = document.createElement('script');
+    gScript.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_KEY}&libraries=places&callback=initMap`;
+    gScript.async = true;
+
+    const funcScript = document.createElement('script');
+    funcScript.type = "text/javascript";
+    
+    const code = `
+      function initMap() {
+        const input = document.getElementById("location-bar");
+
+        let autocomplete = new google.maps.places.Autocomplete(input, {
+          componentRestrictions: {"country": ["us"]},
+          fields: ["formatted_address"]
+        });
+        autocomplete.addListener("place_changed", () => {
+
+          let setValue = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, 'value').set;
+          const userSelect = input.value;
+          console.log("value is " + input.value);
+          setValue.call(input, userSelect);
+          input.dispatchEvent(new Event('input', { bubbles: true }));
+        });
+      }
+    `;
+
+    funcScript.appendChild(document.createTextNode(code));
+    
+    document.body.appendChild(funcScript);
+    document.body.appendChild(gScript);
+
+    // Remove google scripts from DOM
+    return () => {
+      window.google = null;
+      let keywords = ['maps.googleapis'];
+      let scripts = document.head.getElementsByTagName("script");
+      for (let i = scripts.length - 1; i >= 0; i--) {
+        let scriptSource = scripts[i].getAttribute('src');
+        if (scriptSource != null) {
+            if (keywords.filter(item => scriptSource.includes(item)).length) {
+                scripts[i].remove();
+            }
+        }
+      }
+
+      const searchContainer = document.getElementsByClassName("pac-container")[0];
+      document.body.removeChild(gScript);
+      document.body.removeChild(funcScript);
+      document.body.removeChild(searchContainer);
+    }
+  }, []);
 
   return (
     <div className="CreateRoom">
@@ -46,6 +101,7 @@ function CreateRoom() {
             className="CreateRoom__location__input"
             onChange={(e) => setLocation(e.target.value)}
             value={location}
+            placeholder = "Search..."
           />
           <div className="tooltip">
             <img
@@ -66,7 +122,7 @@ function CreateRoom() {
         <div className="CreateRoom__friends__number">
           <div
             className="CreateRoom__friends__number__increment"
-            onClick={onClickInclement}
+            onClick={onClickIncrement}
           >
             +
           </div>
@@ -105,6 +161,8 @@ function CreateRoom() {
         return false;
       }
     }
+    console.log("NAME IS " + name);
+    console.log("LOCATION IS " + location);
 
     return true;
   }
@@ -118,18 +176,35 @@ function CreateRoom() {
         .then(data => {
           const results = data["results"];
           const locationBar = document.getElementById("location-bar");
+
+          let setValue = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, 'value').set;
           
           if (results.length == 0) {
-            locationBar.value = "Unable to determine location."
+            setValue.call(locationBar, "Unable to determine location.");
           }
           else {
-            locationBar.value = results[0]["formatted_address"];
+            const fullAddress = results[0]["formatted_address"];
+            const address_components = results[0]["address_components"];
+
+            let zipcode;
+            for (let i = 0; i < address_components.length; i++) {
+              for (let j = 0; j < address_components[i].types.length; j++) {
+                if (address_components[i].types[j] == "postal_code") {
+                  zipcode = address_components[i]["short_name"];
+                  break;
+                }
+              }
+            }
+            console.log("THE ZIPCODE IS " + zipcode);
+            setValue.call(locationBar, fullAddress);
           }
+
+          locationBar.dispatchEvent(new Event('input', { bubbles: true }));
         });
     });
   }
 
-  function onClickInclement() {
+  function onClickIncrement() {
     if (capacity >= MAX_CAPACITY) return;
     setCapacity(capacity + 1);
   }
