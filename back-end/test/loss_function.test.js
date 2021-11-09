0353a83e0a51377b4facbb309bb9f14a11d0fe3a
thomastai1666 <36852809+thomastai1666@users.noreply.github.com>
2021-11-08 23:30:21 -0500

    #74: Implement loss function

---
+++ back-end/test/loss_function.test.js
@@ -0,0 +1,38 @@
+const assert = require("assert");
+const restauraunts = require("../data/restauraunts.json");
+const user_preferences = require("../data/user_preference.json");
+const {
+  generate_restuaraunts_map,
+  loss_function,
+} = require("../utils/loss_function");
+
+describe("Loss Function Works", function () {
+  describe("generate_restuaraunts_map function works", function () {
+    const expected_result = {
+      4072332573989656: { restaurant_name: "Dallas Jones BBQ", points: 0 },
+      4072055173984543: { restaurant_name: "Ivan Ramen", points: 0 },
+      4071913973985926: { restaurant_name: "Stop 1 Deli", points: 0 },
+    };
+    it("returns expected list of restauraunts", function () {
+      // test that assertion
+      assert.deepEqual(
+        expected_result,
+        generate_restuaraunts_map(restauraunts)
+      );
+    });
+  });
+  describe("loss_function works", function () {
+    const expected_result = {
+      4072332573989656: { restaurant_name: "Dallas Jones BBQ", points: 7 },
+      4072055173984543: { restaurant_name: "Ivan Ramen", points: 2 },
+      4071913973985926: { restaurant_name: "Stop 1 Deli", points: 0 },
+    };
+    it("returns expected list of restauraunts and points", function () {
+      // test that assertion
+      assert.deepEqual(
+        expected_result,
+        loss_function(restauraunts, user_preferences)
+      );
+    });
+  });
+});
---
+++ back-end/utils/loss_function.js
@@ -0,0 +1,71 @@
+const restauraunts_data = require("../data/restauraunts.json");
+const user_preferences_data = require("../data/user_preference.json");
+
+const generate_restuaraunts_map = (restauraunts) => {
+  // Generates a list of restauraunt ids to their point values,
+  // Initialized to zero
+  const data = Object.fromEntries(
+    restauraunts.data.map((restauraunt) => {
+      return [
+        restauraunt.restaurant_id,
+        {
+          restaurant_name: restauraunt.restaurant_name,
+          points: 0,
+        },
+      ];
+    })
+  );
+  return data;
+};
+
+const check_menu = (menus, dishes) => {
+  // Returns number of dishes matching menu
+  let matches = 0;
+  menus.forEach((menu) => {
+    menu.menu_sections.forEach((menu_section) => {
+      menu_section.menu_items.forEach((menu_item) => {
+        dishes.forEach((dish) => {
+          if (dish === menu_item.name) {
+            matches++;
+          }
+        });
+      });
+    });
+  });
+  return matches;
+};
+
+const loss_function = (restauraunts, user_preferences) => {
+  let restauraunt_ranking = generate_restuaraunts_map(restauraunts);
+  restauraunts.data.forEach((restauraunt) => {
+    user_preferences.forEach((user) => {
+      // Match user price preference
+      if (user.price === restauraunt.price_range) {
+        restauraunt_ranking[restauraunt.restaurant_id]["points"] += 1;
+      }
+      // Match user cuisine preference
+      user.cuisines.forEach((cuisine) => {
+        if (restauraunt.cuisines.indexOf(cuisine) != -1) {
+          restauraunt_ranking[restauraunt.restaurant_id]["points"] += 1;
+        }
+      });
+      // Match user menu item preference
+      restauraunt_ranking[restauraunt.restaurant_id]["points"] += check_menu(
+        restauraunt.menus,
+        user.dishes
+      );
+    });
+  });
+  return restauraunt_ranking;
+};
+
+const restauraunt_rankings = () => {
+  // TODO: Return List of Restauraunts
+  let results = [];
+  return results;
+};
+
+exports.restauraunt_rankings = restauraunt_rankings;
+exports.generate_restuaraunts_map = generate_restuaraunts_map;
+exports.check_menu = check_menu;
+exports.loss_function = loss_function;
