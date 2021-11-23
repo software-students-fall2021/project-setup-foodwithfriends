cf2654a40d4eb813e2e34b0cdb526ca999d5d8ec
Tanya Singh <ts3516@nyu.edu>
2021-11-22 21:46:50 -0500

    added reload button

---
+++ front-end/src/pages/Error 3.jsx
@@ -0,0 +1,13 @@
+import './Error.css';
+
+import React from 'react';
+
+function Error() {
+  return (
+    <div className="Error">
+      <h1 id="heading">SOMETHING WENT WRONG</h1>
+    </div>
+  );
+}
+
+export default Error;
