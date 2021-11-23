548c74b100f6f067d1a022cfdbf72d02f1742b6f
Jen-Lopez <jennlolopz@gmail.com>
2021-11-22 23:52:04 -0500

    comment out documenu for now

--- back-end/app.js
+++ back-end/app.js
@@ -17,7 +17,7 @@ const sessionOptions = {
 const roomRoutes = require("./routes/room");
 const userRoutes = require("./routes/user");
 const restRoutes = require("./routes/restaurant");
-const documenuRoutes = require("./routes/documenu");
+// const documenuRoutes = require("./routes/documenu");
 
 // middlewares
 app.use(express.urlencoded({ extended: true }));
@@ -31,7 +31,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(roomRoutes);
 app.use(userRoutes);
 app.use(restRoutes);
-app.use(documenuRoutes);
+// app.use(documenuRoutes);
 
 const env = process.env.NODE_ENV || 'development';
 const port = env === "test" ? 80 : 8000;
