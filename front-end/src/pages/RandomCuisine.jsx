c4bc40c220c252e0bf6baeb2c86ad64f5cda0ee5
Tanya Singh <ts3516@nyu.edu>
2021-11-22 21:07:43 -0500

    fixed some errors

--- front-end/src/pages/RandomCuisine.jsx
+++ front-end/src/pages/RandomCuisine.jsx
@@ -1,22 +1,26 @@
 import './RandomCuisine.css';
-
 import React from 'react';
-import Button from '../components/Button'
+import data from '../data/cuisines.json';
 
-function RandomCuisine() {
-  return (
+
+
+function RandomCuisine() {     
+  const cuisineData = Object.values(data);
+  const generateRand = cuisineData[parseInt(Math.random() * cuisineData.length)];
+  const randomCuisine = generateRand;
+  console.log(randomCuisine);
+    return (
     <div className="RandomCuisine">
-      <div id="random-cuisine-title-top"> 
-        <h1>Random Cuisine</h1>
+      <div id="random-cuisine-title-top"> {randomCuisine.name}</div>
+      <img id="cuisine-photo"src="" className="food" alt=""></img>
+      <div id="cuisine-text">
+      {randomCuisine.description}
       </div>
-      <div> <h3>Name</h3></div>
-      <div id="cuisine-photo"></div>
-      <div id = "description-container">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <div id="vote-button-div">
+        <button id="vote-button"> Vote </button>
       </div>
-
-      <Button id = "vote-button" text="Vote" width="300px" height="50px"/>
     </div>
+
   );
 }
 
