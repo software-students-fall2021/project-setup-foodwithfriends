1d176b7cc7f4802c83b522eb70e2822c739b2ae4
Tanya Singh <ts3516@nyu.edu>
2021-11-29 20:01:12 -0500

    fixed some linting issues

--- front-end/src/pages/RandomCuisine.jsx
+++ front-end/src/pages/RandomCuisine.jsx
@@ -11,12 +11,8 @@ function RandomCuisine() {
   const cuisineData = Object.values(data);
   const generateRand = cuisineData[parseInt(Math.random() * cuisineData.length)];
   const randomCuisine = generateRand;
-  console.log(randomCuisine);
   return (
     <div className="RandomCuisine">
-      
-      
-    
       <div id="random-cuisine-title-top"> {randomCuisine.name}</div>
       <img id="cuisine-photo" src={require(`../img/cuisines/${randomCuisine.cuisine}/${randomCuisine.thumbnail}`).default} className="food" alt=""></img>
       <div id="cuisine-text">
