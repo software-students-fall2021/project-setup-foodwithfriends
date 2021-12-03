555a7a029c1e64cd3c6ee7aacd1a6ae04a277bc2
Jen-Lopez <jennlolopz@gmail.com>
2021-12-03 16:28:23 -0500

    update css for /win

--- front-end/src/pages/WinningCuisine.jsx
+++ front-end/src/pages/WinningCuisine.jsx
@@ -40,6 +40,7 @@ function WinningCuisine(props) {
   
   return (
     <div className="WinningCuisine">
+      <h1 id="winner">WINNER</h1>
       <div className="WinningImage"><img id="winImg" src={winningLogo} alt="Winning cuisine image"/></div>
       <div className="WinningHeader">{props.cuisine}Chosen Cuisine Name</div>
       <p className="WinningFacts">{props.description}Facts about cuisine. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.</p>
