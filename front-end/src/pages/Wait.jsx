02f9d3b3e172f3347981b9b528ccc10ab8f1822f
Thomas Tai <36852809+thomastai1666@users.noreply.github.com>
2021-12-03 12:52:54 -0500

    Update front-end/src/pages/Wait.jsx

--- front-end/src/pages/Wait.jsx
+++ front-end/src/pages/Wait.jsx
@@ -8,7 +8,7 @@ import React from 'react';
 function Wait() {
   const [users, setUsers] = React.useState("")
   const [userTotal, setUserTotal] = React.useState("?")
-  const [groupId, setName] = React.useState("b6034"); //THIS IS TEMPORARY JUST FOR NOW
+  const [groupId, ] = React.useState("b6034"); //THIS IS TEMPORARY JUST FOR NOW
   const [friends, setFriends] = React.useState([])
   const checkUser = async () => {
     const users = await get(
