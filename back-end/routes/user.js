ed119cac33588200c8d654c4a2858b54036279a2
Thomas Tai <36852809+thomastai1666@users.noreply.github.com>
2021-12-07 21:26:58 -0500

    #122: Use express validator on room, user, and wait

--- back-end/routes/user.js
+++ back-end/routes/user.js
@@ -1,12 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const Group = require("../models/group");
+const { body, validationResult } = require("express-validator");
 
-router.post("/user", function (req, res) {
+router.post(
+  "/user",
+  body("userName").isString(),
+  body("groupID").isString(),
+  function (req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
     const name = req.body.userName;
     const id = req.body.groupID;
-    const newUser = new User({groupId:id, name: name, dishPreferences: []});
+    const newUser = new User({ groupId: id, name: name, dishPreferences: [] });
 
     newUser.save((err, result) => {
       if (err) {
@@ -15,19 +25,25 @@ router.post("/user", function (req, res) {
         res.send({ success: false });
         return;
       }
-    //   console.log(result);
-      Group.findOneAndUpdate({groupId: id}, {$addToSet: {friends: newUser}}, {new: true}, (err, doc) => {
-        if (err) {
+      //   console.log(result);
+      Group.findOneAndUpdate(
+        { groupId: id },
+        { $addToSet: { friends: newUser } },
+        { new: true },
+        (err, doc) => {
+          if (err) {
             // console.log("Something wrong when updating the data");
             res.status(500);
             res.send({ success: false });
-            return
+            return;
+          }
+          // console.log(doc);
+          res.status(200);
+          res.send({ success: true, userID: result._id });
         }
-        // console.log(doc);
-        res.status(200);
-        res.send({ success: true , userID: result._id});
-      });
-    })
-});
+      );
+    });
+  }
+);
 
 module.exports = router;
