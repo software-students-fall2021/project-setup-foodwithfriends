09f564ae7dbcb74159fdac1bab6cb392e78c2ecc
Thomas Tai <36852809+thomastai1666@users.noreply.github.com>
2021-12-07 21:26:29 -0500

    #122: Use express validator on cuisine and dishes

--- back-end/routes/dishes.js
+++ back-end/routes/dishes.js
@@ -1,21 +1,35 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
+const { body, validationResult } = require("express-validator");
 
-router.post("/preferred", function (req, res) {
+router.post(
+  "/preferred",
+  body("userID").isString(),
+  body("dish").isArray(),
+  function (req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
     const userID = req.body.userID;
     const dish = req.body.dish;
-    User.findOneAndUpdate({_id: userID},{$push:{ dishPreferences: dish}},(err, doc) => {
+    User.findOneAndUpdate(
+      { _id: userID },
+      { $push: { dishPreferences: dish } },
+      (err, doc) => {
         if (err) {
-            console.log("Something wrong when finding the group");
-            res.status(500);
-            res.send(err);
-            return;
+          console.log("Something wrong when finding the group");
+          res.status(500);
+          res.send(err);
+          return;
         }
         res.status(200);
-        res.send({valid: true});
-    });
-});
+        res.send({ valid: true });
+      }
+    );
+  }
+);
 
 module.exports = router;
